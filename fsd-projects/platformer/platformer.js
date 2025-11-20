e1ac3917b2818cbe666cc6cd46ecaa82462e7ed6
dakaraiperkins-prog <dakarai.perkins@inspire35.org>
2025-11-20 21:32:41 +0000

    message

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -17,31 +17,42 @@ $(function () {
     }
 
     // Create walls - do not delete or modify this code
-    createPlatform(-50, -50, canvas.width + 100, 50); // top wall
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "navy"); // bottom wall
-    createPlatform(-50, -50, 50, canvas.height + 500); // left wall
-    createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
-
+    
     //////////////////////////////////
     // ONLY CHANGE BELOW THIS POINT //
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     toggleGrid();
+    //
+    
+
 
 
     // TODO 2 - Create Platforms
+  
+createPlatform(300,600,200,10,"black"); 
+   createPlatform(0,700,200,10,"blue");
+   createPlatform(500,500,200,10,"red");
+   createPlatform(700,600,200,10,"pink");
+   createPlatform(900,700,200,10,"yellow");
+   createPlatform(1200,700,200,10,"green");
 
 
 
 
     // TODO 3 - Create Collectables
+createCollectable("database",200,660);
+    createCollectable("database",600,460);
+    createCollectable("database",1000,660);
 
 
 
     
     // TODO 4 - Create Cannons
-
+createCannon("top", 300,2000);
+        createCannon("bottom", 545,1350);
+            createCannon("bottom", 900,2300);
 
     
     
